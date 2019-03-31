b7a7944536830d0223827f5c7eea6afad1a540c8
Jeremy-san <shehata.jeremy@gmail.com>
2019-03-31 23:36:41 +0200

    correction exercice 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { HighlightDirective } from './highlight.directive';
 import { MoviesComponent } from './movies/movies.component';
+import { DisplayGuestsDirective } from './display-guests.directive';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { MoviesComponent } from './movies/movies.component';
     SignUpComponent,
     UserProfileComponent,
     HighlightDirective,
-    MoviesComponent
+    MoviesComponent,
+    DisplayGuestsDirective
   ],
   imports: [
     BrowserModule,
